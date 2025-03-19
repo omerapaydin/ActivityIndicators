566d93481e06944132b1aecf59108a6ff2b17bbb
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-19 23:05:56 +0300

    First

--- ActivityIndicators/ViewController.swift
+++ ActivityIndicators/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        indicator.isHidden = true
     }
 
-
+    @IBAction func basla(_ sender: Any) {
+        indicator.startAnimating()
+        indicator.isHidden = false
+    }
+    
+    @IBAction func dur(_ sender: Any) {
+        indicator.stopAnimating()
+        indicator.isHidden = true
+    }
 }
 
