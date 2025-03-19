576646fb82634a974655018e096f932b74be5475
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-19 22:57:42 +0300

    Initial Commit

---
+++ ActivityIndicatorsTests/ActivityIndicatorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityIndicatorsTests.swift
+//  ActivityIndicatorsTests
+//
+//  Created by Ömer on 19.03.2025.
+//
+
+import Testing
+@testable import ActivityIndicators
+
+struct ActivityIndicatorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
